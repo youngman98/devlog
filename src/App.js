28f8d4eb036ad5011671298081d8722002a2b6b5
김영인 <youngman0908@naver.com>
2023-10-12 01:29:57 +0900

    메인페이지 퍼블리싱 HTML/CSS(다크모드빼고 거의 완성), :root, reset.css 적용

--- src/App.js
+++ src/App.js
@@ -6,37 +6,36 @@ function App() {
       {/* header */}
       <div className="Header">
         <div className="Header-container">
-          <div className="Header-logo">로고</div>
+          <a href=""><div className="Header-logo"></div></a>
           <ul className="Header-navbar">
-            <li>contact</li>
-            <li>github</li>
-            <li>portfolio</li>
+            <li>Contact</li>
+            <li>Github</li>
+            <li>Portfolio</li>
           </ul>
         </div>
       </div>
 
-      <div className="White-space height-large"></div>
       <div className="Content-container">
 
       {/* white-space */}
 
       {/* sidebar */}
       <div className="Sidebar-container">
-        <div className="Sidebar-profile-img">
-          
-        </div>
+        <div className="Sidebar-profile-img"></div>
         <div className="Sidebar-profile-info">
-          <p className="font-large">YoungIn</p>
-          <p className="font-small">Front-end dev</p>
+          <hr/>
+          <p className="Font-large">YoungIn</p>
+          <p className="Font-small">Front-end dev</p>
+          <hr/>
         </div>
         <ul className="Sidebar-menu">
-          <li><a href="">HTML/CSS</a></li>
-          <li><a href="">Javascript</a></li>
-          <li><a href="">Typescript</a></li>
-          <li><a href="">React</a></li>
-          <li><a href="">Git</a></li>
-          <li><a href="">CS</a></li>
-          <li><a href="">Algorithm</a></li>
+          <li><a href=""><div className="Sidebar-icon"></div>HTML/CSS <span className="Sidebar-postCount">20</span></a></li>
+          <li><a href=""><div className="Sidebar-icon"></div>Javascript</a></li>
+          <li><a href=""><div className="Sidebar-icon"></div>Typescript</a></li>
+          <li><a href=""><div className="Sidebar-icon"></div>React</a></li>
+          <li><a href=""><div className="Sidebar-icon"></div>Git</a></li>
+          <li><a href=""><div className="Sidebar-icon"></div>CS</a></li>
+          <li><a href=""><div className="Sidebar-icon"></div>Algorithm</a></li>
         </ul>
       </div>
 
@@ -45,8 +44,9 @@ function App() {
         {/* search */}
         <div className="Search">
           <input type="text" placeholder="Search"/>
-          <button className="Search-icon"></button>
+          <div className="Search-icon"></div>
         </div>
+        <div className="Darkmode-toggle-container"></div>
         <div className="PostList">
           <div className="Post-container">
             <a href="">
@@ -77,9 +77,92 @@ function App() {
                 <p className="Post-date">
                   2023. 10. 10
                 </p>
+                <p className="Post-"></p>
               </div>
             </a>
           </div>
+          <div className="Post-container">
+            <a href="">
+              <div className="Post">
+                <div className="Thumbnail">
+                  <img src="https://user-images.githubusercontent.com/75000376/273954097-0b7bfc4b-b86c-4eab-9e25-50bd830f4aac.png" alt="" />
+                </div>
+                <div className="Post-inner">
+                  <span>[React]</span>
+                  <p>JSX란?</p>
+                </div>
+                <p className="Post-date">
+                  2023. 10. 10
+                </p>
+              </div>
+            </a>
+          </div>
+          <div className="Post-container">
+            <a href="">
+              <div className="Post">
+                <div className="Thumbnail">
+                  <img src="https://user-images.githubusercontent.com/75000376/273954097-0b7bfc4b-b86c-4eab-9e25-50bd830f4aac.png" alt="" />
+                </div>
+                <div className="Post-inner">
+                  <span>[React]</span>
+                  <p>JSX란?</p>
+                </div>
+                <p className="Post-date">
+                  2023. 10. 10
+                </p>
+              </div>
+            </a>
+          </div>
+          <div className="Post-container">
+            <a href="">
+              <div className="Post">
+                <div className="Thumbnail">
+                  <img src="https://user-images.githubusercontent.com/75000376/273954097-0b7bfc4b-b86c-4eab-9e25-50bd830f4aac.png" alt="" />
+                </div>
+                <div className="Post-inner">
+                  <span>[React]</span>
+                  <p>JSX란?</p>
+                </div>
+                <p className="Post-date">
+                  2023. 10. 10
+                </p>
+              </div>
+            </a>
+          </div>
+          <div className="Post-container">
+            <a href="">
+              <div className="Post">
+                <div className="Thumbnail">
+                  <img src="https://user-images.githubusercontent.com/75000376/273954097-0b7bfc4b-b86c-4eab-9e25-50bd830f4aac.png" alt="" />
+                </div>
+                <div className="Post-inner">
+                  <span>[React]</span>
+                  <p>JSX란?</p>
+                </div>
+                <p className="Post-date">
+                  2023. 10. 10
+                </p>
+              </div>
+            </a>
+          </div>
+          <div className="Post-container">
+            <a href="">
+              <div className="Post">
+                <div className="Thumbnail">
+                  <img src="https://user-images.githubusercontent.com/75000376/273954097-0b7bfc4b-b86c-4eab-9e25-50bd830f4aac.png" alt="" />
+                </div>
+                <div className="Post-inner">
+                  <span>[React]</span>
+                  <p>JSX란?</p>
+                </div>
+                <p className="Post-date">
+                  2023. 10. 10
+                </p>
+              </div>
+            </a>
+          </div>
+          
+          
         </div>
       </div>
       </div>
